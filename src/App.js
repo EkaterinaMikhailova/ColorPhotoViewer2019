58dc2c6a717810345925801d437148d88f59d2ad
Кристина Кутузова <kutuzovakristino@mail.ru>
2019-03-15 20:31:52 +0300

    Fixed errors in the alpha version of the project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react';
-import './App.css';
 import Cap from "./components/cap";
 import Bottom from "./components/bottom";
 import FileInput from "./components/inputfile";
 import PhotoEdit from "./components/photoEdit";
+import Photo from "./components/photo";
+import Filtres from "./components/filtres";
+import Save from "./components/save";
 
 class App extends Component{
-    render(){ 
+  render(){ 
     return(
-        <div>
-            <Cap />
-            <FileInput />
-            <PhotoEdit/>
-            <div id="about">
-                <div class="title">
-                    <h2>О текстовом редакторе</h2>
-                </div >
-                <p class="text-center">Онлайн редактор фотографий для ПК. В нашем редакторе большой выбор фильтров для фото. Вы можете выбрать одно из предложенных фото или загрузить своё.</p> 
-                <footer class="footer"></footer>
-            </div>
-            <Bottom/>
+      <div>
+        <div id="home">
+          <Cap />
+          <br/>
+          <FileInput />
+          <PhotoEdit/>
+          <br/>
         </div>
-        );
-    }
+        <br/><br/>
+        <div id="processing">
+          <div class="container">
+            <div class="row">
+              <Photo />
+            </div> 
+            <div class="row">
+              <Filtres />
+            </div> 
+          </div>
+          <br/>
+          <Save/>
+        </div>
+        <div id="about">
+          <div class="title">
+            <h2>О текстовом редакторе</h2>
+          </div >
+          <p class="text-center">Онлайн редактор фотографий для ПК. В нашем редакторе большой выбор фильтров для фото. Вы можете выбрать одно из предложенных фото или загрузить своё.</p> 
+          <footer class="footer"></footer>
+        </div>
+        <Bottom />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/filtres.js
+++ src/components/filtres.js
@@ -5,92 +5,92 @@ class Filtres extends Component{
     return(
       <div id="dg">
         <table class="table">
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Amaro </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Brannan</h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Earlybird </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Hefe </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Hudson </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Inkwell </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Lomo </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Myfair </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Rise </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Sierra </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Toster </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Valencia </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>Willow </h5>
-              </div>
-            </th>
-            <th>
-              <div class="tilt">
-                <a href="_blank"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
-                <h5>XPro</h5>
-              </div>
-            </th>
-      </table>
-    </div>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Amaro </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Brannan</h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Earlybird </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Hefe </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Hudson </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Inkwell </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Lomo </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Myfair </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Rise </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Sierra </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Toster </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Valencia </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>Willow </h5>
+            </div>
+          </th>
+          <th>
+            <div class="tilt">
+              <a href="#home"><img src="https://img.icons8.com/cotton/64/000000/camera.png" alt=""/></a>
+              <h5>XPro</h5>
+            </div>
+          </th>
+        </table>
+      </div>
     );
   }
 }
